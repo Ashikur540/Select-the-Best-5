e5e915b46e286ea556ddbb3f35a37d86ac120e25
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-08-23 00:27:14 +0600

    js added and button function added

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,7 @@
-console.log("conned!");
+
+
+function addPlayers(button) {
+    console.log('added!!');
+    button.setAttribute("disabled", "");
+    button.style.backgroundColor = 'rgba(180, 176, 176, 0.2)'
+}
