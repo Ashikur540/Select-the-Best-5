d22a89712027c56ed8e2884b7139580342e21877
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-08-23 00:42:12 +0600

    players name and soime functions added

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,26 @@
-
+const players = [];
 
 function addPlayers(button) {
-    console.log('added!!');
+    //    disabling btn
     button.setAttribute("disabled", "");
-    button.style.backgroundColor = 'rgba(180, 176, 176, 0.2)'
+    button.style.backgroundColor = 'rgba(180, 176, 176, 0.2)';
+    // accessing trhe elements
+    const plName = (button.parentNode.children[0].innerText);
+
+    // pushing players to array
+    players.push(plName);
+    console.log(players);
+    // updsating count
+    const pltotal = document.getElementById('total-players');
+    pltotal.innerText = `Selected Five ->  ${players.length} ✅`;
+    // upading list dynamically
+    for (let i = 0; i < players.length; i++) {
+        console.log(players[i]);
+
+    }
+    const plList = document.getElementById('ol-body');
+    plList.innerHTML = ` <li>players[i]</li>
+    <li>players[i]</li>
+    <li>players[i]</li>
+    <li>players[i]</li>`
 }
