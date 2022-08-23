d9387ab06738b16d6e9d9f06625330b4d12e6e93
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-08-23 20:32:54 +0600

    all functions and validations completed

--- Js/app.js
+++ Js/app.js
@@ -1,71 +1,98 @@
-const players = [];
+// getting the buttons and labels to show result
+const plListBody = document.getElementById('ol-body');
+const plCostBtnEle = document.getElementById('plCostCalc');
 
+const TotalCostBtnEle = document.getElementById('totalCost');
+const plCostTotalEle = document.getElementById('plExp');
+const FinalTotalCostEle = document.getElementById('CostTotal');
+const noplayerlabel = document.getElementById('noplayer')
+
+
+
+// utility function to acces the input values
+
+function getInputs(eleId) {
+    const ele = document.getElementById(eleId);
+    const eleValue = parseFloat(ele.value);
+    return eleValue;
+}
+
+const players = [];
+// function to select players
 function addPlayers(button) {
     //    disabling btn
     button.setAttribute("disabled", "");
     button.style.backgroundColor = 'rgba(180, 176, 176, 0.2)';
     // accessing trhe elements
     const plName = (button.parentNode.children[0].innerText);
-    const plListBody = document.getElementById('ol-body');
+    noplayerlabel.style.display = "none";
     // pushing players to array
     players.push(plName);
 
-
+    // condition for maximum 5 players
     if (players.length <= 5) {
         const pltotal = document.getElementById('total-players');
-        pltotal.innerText = `Selected Five ->  ${players.length} ✅`;
+        pltotal.innerText = `Selected  ->  ${players.length} ✅`;
         // upading list dynamically
         plListBody.innerHTML = ""
         for (let i = 0; i < players.length; i++) {
-            // players.push(plName);
             const playerNamee = players[i];
-            // console.log('pl name', playerNamee);
-
-
             const plList = document.createElement('h4')
-            plList.innerHTML = ` <h4>${i + 1}- ${playerNamee}</h4>
+            plList.innerHTML = ` <h4>${i + 1} - &nbsp; ${playerNamee}</h4>
             `
             plListBody.appendChild(plList);
-
-
         }
 
     }
 
     else {
-        alert("You can slect only 5 players");
+        alert("    Sorry 😢\n You can select only 5 players");
         players.length--;
         return;
     }
-    // updsating count
-
-
 }
 
 
-const plCostBtnEle = document.getElementById('plCostCalc');
-const TotalCostBtnEle = document.getElementById('totalCost');
-
-
-
-const plCostEle = document.getElementById('plPcost');
-const plCostTotalEle = document.getElementById('plExp');
-const managCostEle = document.getElementById('managCost');
-const coachCostEle = document.getElementById('coachCost');
-const FinalTotalCostEle = document.getElementById('CostTotal');
+// function to calculate expense of players
 function calculatePlayerExpense() {
-    const perCost = parseFloat(plCostEle.value);
+    // using the utility function 
+    const perCost = getInputs("plPcost");
     const playerCostTotal = perCost * players.length;
     console.log(playerCostTotal);
-    plCostTotalEle.innerText = `Total Players Expense: ${playerCostTotal}`;
+    plCostTotalEle.innerText = `Total Players Expense: $ ${playerCostTotal}`;
     return playerCostTotal;
-}
-plCostBtnEle.addEventListener("click", calculatePlayerExpense)
 
+}
+// calculate players cost through button
+plCostBtnEle.addEventListener("click", function () {
+    const perCost = getInputs("plPcost");
+    // with validation
+    if (!(isNaN(perCost))) {
+        return calculatePlayerExpense();
+    }
+
+    else {
+        alert("         !!!!!Opsss!!! \n you have missed to input per player cost amount👇🙏")
+    }
+});
+
+// calculate other cost within players
 TotalCostBtnEle.addEventListener("click", function TotalCostBtnEle() {
+
+    // using callculatePlayerexpense function as common function in this function
     const previousTotal = calculatePlayerExpense();
-    console.log(previousTotal);
-    const finalTotal = previousTotal + parseFloat((managCostEle.value)) + parseFloat((coachCostEle.value))
-    console.log(finalTotal);
-    FinalTotalCostEle.innerText = `Total Expense: ${finalTotal}`;
+    // using the utility function 
+    const managerCost = getInputs("managCost");
+    const coachCost = getInputs("coachCost");
+    // validation
+    if (!(isNaN(managerCost)) && !(isNaN(coachCost))) {
+        const finalTotal = previousTotal + coachCost + managerCost;
+        FinalTotalCostEle.innerText = `Total Expense: $ ${finalTotal}`;
+    }
+
+    else {
+        alert("Please enter the cost amount of below inputs👇🙏")
+    }
 })
+
+
